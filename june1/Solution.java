bd2d577d87490a3e0bc846872389b6b8c0506c72
Prince Maurya <princemaurya6921@gmail.com>
2026-06-01 21:33:29 +0530

    first commit

---
+++ june1/Solution.java
@@ -0,0 +1,16 @@
+// public class shorts {
+//     public static void main(String args[]){
+//         short x = 5;
+//         x = x * 10;
+//         System.out.println(x);
+//     }
+// }
+
+public class Solution{
+       public static void main(String[] args){
+                     byte x = 127;
+                     x++;
+                     x++;
+                     System.out.print(x);
+       }
+}
