ce972273fb6974fba2bc96297c3824490975d447
Prince Maurya <princemaurya6921@gmail.com>
2026-06-01 22:16:20 +0530

    1 june practice MCQ Question

--- june1/Solution.java
+++ june1/Solution.java
@@ -1,16 +1,29 @@
-// public class shorts {
-//     public static void main(String args[]){
-//         short x = 5;
-//         x = x * 10;
-//         System.out.println(x);
-//     }
+
+//Find the value of A[1] after execution of the following program.
+
+// public class Solution {
+//        public static void main(String[] args) {
+//       int[] A = {0, 2, 4, 1, 3};
+
+
+// for(int i = 0; i < A.length; i++){
+// A[i] = A[(A[i] + 3) % A.length];
+// }
+//        }
 // }
 
-public class Solution{
-       public static void main(String[] args){
-                     byte x = 127;
-                     x++;
-                     x++;
-                     System.out.print(x);
-       }
+
+// public class Solution {
+//        public static void main(String[] args) {
+//               int ++a = 100;
+//               System.out.println(++a);
+//        }
+// }
+
+public class Solution {
+    public static void main(String args[]){
+        int x = 5;
+        x *= 3 + 7;
+        System.out.println(x);
+    }
 }
