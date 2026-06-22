a2549889470fdc838f1f83ab52607b2e3bbf083d
Prince Maurya <princemaurya6921@gmail.com>
2026-06-22 16:00:11 +0530

    22

--- time space complexity/bubblesort.java
+++ time space complexity/bubblesort.java
@@ -1,12 +1,12 @@
-public class sortingBubble {
-    public static void main(String[] args) {
-        int arr[] = {5, 4, 3, 2, 1}; // Worst case
-        bubbleSort(arr);
-        System.out.println("Sorted array in worst case:");
-        printArray(arr);
+// public class sortingBubble {
+//     public static void main(String[] args) {
+//         int arr[] = {5, 4, 3, 2, 1}; // Worst case
+//         bubbleSort(arr);
+//         System.out.println("Sorted array in worst case:");
+//         printArray(arr);
 
-        int arr2[] = {1, 2, 3, 4, 5}; // Best case
-        bubbleSort(arr2);
-        System.out.println("Sorted array in best case:");
-        printArray(arr2);
-    }
+//         int arr2[] = {1, 2, 3, 4, 5}; // Best case
+//         bubbleSort(arr2);
+//         System.out.println("Sorted array in best case:");
+//         printArray(arr2);
+//     }
