5b4baa6c84317367b672dd77888f6fa3651941ca
Prince Maurya <princemaurya6921@gmail.com>
2026-07-01 00:38:16 +0530

    1 jul

--- Array List 28-06/swaps.java
+++ Array List 28-06/swaps.java
@@ -1,5 +1,10 @@
 import java.util.ArrayList;
-public record AL() {
+public class swaps {
+    public static void swaps(ArrayList<Integer> list, int idx1, int idx2) {
+        int temp = list.get(idx1);
+        list.set(idx2, temp);
+    }
+
     public static void main(String args[]) {
 
         //Classname Objectname = new Classname();
@@ -16,7 +21,7 @@ public record AL() {
        int idx1 = 1;
        int idx2 = 3;
        System.out.println(list);
-       swap(list, idx1, idx2);
+       swaps(list, idx1, idx2);
        System.out.println(list);
         }
     }
